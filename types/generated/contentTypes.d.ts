85c2931e8c10ed563a88e894e562ba20c2cda30c
Shiv Singh <shivsingh7150@hotmail.com>
2024-01-07 19:09:27 +0530

    add testimonials collection

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -1161,6 +1161,39 @@ export interface ApiPostCategoryPostCategory extends Schema.CollectionType {
   };
 }
 
+export interface ApiTestimonialTestimonial extends Schema.CollectionType {
+  collectionName: 'testimonials';
+  info: {
+    singularName: 'testimonial';
+    pluralName: 'testimonials';
+    displayName: 'Testimonial';
+  };
+  options: {
+    draftAndPublish: true;
+  };
+  attributes: {
+    name: Attribute.String;
+    position: Attribute.String;
+    slug: Attribute.UID<'api::testimonial.testimonial', 'name'>;
+    excerpt: Attribute.Blocks;
+    createdAt: Attribute.DateTime;
+    updatedAt: Attribute.DateTime;
+    publishedAt: Attribute.DateTime;
+    createdBy: Attribute.Relation<
+      'api::testimonial.testimonial',
+      'oneToOne',
+      'admin::user'
+    > &
+      Attribute.Private;
+    updatedBy: Attribute.Relation<
+      'api::testimonial.testimonial',
+      'oneToOne',
+      'admin::user'
+    > &
+      Attribute.Private;
+  };
+}
+
 declare module '@strapi/types' {
   export module Shared {
     export interface ContentTypes {
@@ -1188,6 +1221,7 @@ declare module '@strapi/types' {
       'api::portfolio-category.portfolio-category': ApiPortfolioCategoryPortfolioCategory;
       'api::post.post': ApiPostPost;
       'api::post-category.post-category': ApiPostCategoryPostCategory;
+      'api::testimonial.testimonial': ApiTestimonialTestimonial;
     }
   }
 }
